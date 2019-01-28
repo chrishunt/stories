131c780b3fe8d89b5c703b835c3410feed55b2cb
Chris Hunt <chris.hunt@coinbase.com>
2019-01-27 20:07:51 -0800

    Decrease max width to 1700

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: `2000px`,
+            maxWidth: `1700px`,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
